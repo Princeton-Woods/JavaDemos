ef70ec4390e6adda666b8e6e44c1d9e836f1f6aa
princeton woods <vpprinceton@aol.com>
2021-07-29 17:18:46 -0400

    Hash map practice finished

--- DEMOS/src/com/princeton/Hashprac.java
+++ DEMOS/src/com/princeton/Hashprac.java
@@ -1,8 +1,11 @@
 package com.princeton;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Map;
 
 public class Hashprac {
 	public static void main(String[] args) {
@@ -23,5 +26,26 @@ public class Hashprac {
 		
 		set2.add(null);
 		set2.add(null);
+		
+		
+		////Assignment
+		
+        Map<Integer, String> student = new HashMap<>();
+        student.put(1, "Mark");
+        student.put(2, "Paul");
+        student.put(3, "John");
+        //size
+        System.out.println(student.size());
+        //copy
+        Map<Integer, String> studentCopy = CopyMap(student);
+        System.out.println(studentCopy);
 	}
+	public static <K, V> Map<K, V> CopyMap(Map<K, V> original)
+    {
+        Map<K, V> second_Map = new HashMap<>();
+        for (Map.Entry<K, V> entry : original.entrySet()) {
+            second_Map.put(entry.getKey(), entry.getValue());
+        }
+        return second_Map;
+    }
 }
